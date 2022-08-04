f67f5eca067b574ea2ef87a7d60aff8cb70d5063
Sergei Kononov <kobo220376@gmail.com>
2022-08-04 15:42:55 +0300

    clean up the project, stuctured and added the folders, added initial assets and fonts

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>text_runner</h1>
+      <h2>
+        by <a href="#">Kononov Sergey</a>
+      </h2>
+    </header>
+  );
+};
+
+export default Header;
